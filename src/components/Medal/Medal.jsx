f7fda3a9ec4fea991e80d2921e1e575207ab3325
vitor.andreylopess@gmail.com <vitor.andreylopess@gmail.com>
2023-03-13 17:44:45 -0300

    comecei a serction laurel

---
+++ src/components/Medal/Medal.jsx
@@ -0,0 +1,17 @@
+import "./medal.css";
+
+import { images } from "../../constants";
+
+function Medal({ img, title, text }) {
+  return (
+    <div className="medal__container">
+      <img src={img} alt="img" />
+      <div className="medal__content">
+        <p className="p__cormorant">{title}</p>
+        <p className="p__opensans">{text}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Medal;
--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React from "react";
+import { SubHeading, Medal } from "../../components";
 
-import './Laurels.css';
+import { images } from "../../constants";
+
+import "./Laurels.css";
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className="app__laurel">
+    <div className="app__laurel-info">
+      <SubHeading title="Awards & recognition" />
+      <h1 className="headtext__cormorant">Our Laurels</h1>
+      <div className="app__laurel-info_container">
+        <Medal
+          img={images.award02}
+          title="Bib Gourmond"
+          text="Lorem ipsum dolor sit amet, consectetur."
+        />
+        <Medal
+          img={images.award01}
+          title="Rising Star"
+          text="Lorem ipsum dolor sit amet, consectetur."
+        />
+        <Medal
+          img={images.award05}
+          title="AA Hospitality "
+          text="Lorem ipsum dolor sit amet, consectetur."
+        />
+        <Medal
+          img={images.award03}
+          title="Outstanding Chef"
+          text="Lorem ipsum dolor sit amet, consectetur."
+        />
+      </div>
+    </div>
+    <div className="app__laurel-image">
+      <img src={images.laurels} alt="" />
+    </div>
   </div>
 );
 
